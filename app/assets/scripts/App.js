a8d095f602eb913e0fcfb2d451fe8117d77c0cd7
Sudeep <sudeep.bhaumick@gmail.com>
2022-06-11 20:37:45 +0530

    Intro to OOP in JS

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,19 @@ import '../styles/styles.css';
 if (module.hot) {
   module.hot.accept();
 }
+
+/** Lesson example code below this line */
+
+import Person from './modules/Person';
+import Adult from './modules/Adult';
+
+let john = new Adult("John Doe", "Blue");
+john.greet();
+john.payTaxes();
+
+let jane = new Adult("Jane Doe", "Yellow");
+jane.greet();
+jane.payTaxes();
+
+let jack = new Person("Jack Sparrow", "Black");
+jack.greet();
