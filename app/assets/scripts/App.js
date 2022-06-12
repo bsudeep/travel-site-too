0133acb85c489f662f1379251b42cbea1a74a46d
Sudeep <sudeep.bhaumick@gmail.com>
2022-06-12 19:52:15 +0530

    Added sticky header function

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import '../styles/styles.css';
 
 import MobileMenu from './modules/MobileMenu';
+import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 let theMobileMenu = new MobileMenu();
+let theStickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
 
