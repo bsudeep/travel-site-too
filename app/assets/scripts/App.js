77cf49c39e6693f35eaf1360ba0fcdc8fb0f7d51
Sudeep <sudeep.bhaumick@gmail.com>
2022-06-13 17:16:59 +0530

    Modalcomplete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,11 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/RevealOnScroll';
+import Modal from './modules/Modal';
 
 let theMobileMenu = new MobileMenu();
 let theStickyHeader = new StickyHeader();
+new Modal();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
 
