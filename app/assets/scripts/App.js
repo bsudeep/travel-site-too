6a3e49b493200acab5bc912f2f22f3500ed5328e
Sudeep <sudeep.bhaumick@gmail.com>
2022-06-13 18:49:10 +0530

    Lazyloading complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,5 @@
 import '../styles/styles.css';
-
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/RevealOnScroll';
